8296829f76c8f35cc419965e9538a049575fc5af
alvarohc777 <aherrada@uninorte.edu.co>
2024-01-19 01:10:02 -0500

    initial commit

---
+++ src/components/ProjectsCards.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="q-pa-md row justify-around q-gutter-md">
+    <q-card class="my-card">
+      <q-img ratio="1.5" fit="fill" src="~assets/faultDetectorApp.png">
+        <div class="absolute-bottom text-h6" style="padding: 0">
+          <q-item
+            clickable
+            style="padding: 16px"
+            tag="detectorDeFallas"
+            target="_blank"
+            href="https://alvarohc777.github.io/"
+          >
+            {{ faultDetectorAPP.name }}
+          </q-item>
+        </div>
+      </q-img>
+
+      <q-card-section>
+        {{ faultDetectorAPP.description }}
+      </q-card-section>
+    </q-card>
+
+    <q-card class="my-card">
+      <q-img ratio="1.5" fit="fill" src="~assets/finaleApp.png">
+        <q-icon
+          class="absolute all-pointer-events"
+          size="32px"
+          name="warning"
+          color="red"
+          style="top: 6px; left: 6px"
+        >
+          <q-tooltip> App on development </q-tooltip>
+        </q-icon>
+        <div class="absolute-bottom text-h6" style="padding: 0">
+          <q-item
+            clickable
+            style="padding: 16px"
+            tag="finaleApp"
+            target="_blank"
+            href="https://finaleapp.azurewebsites.net/"
+          >
+            {{ finaleAPP.name }}
+          </q-item>
+        </div>
+      </q-img>
+
+      <q-card-section>
+        {{ finaleAPP.description }}
+      </q-card-section>
+    </q-card>
+
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <div class="absolute-bottom text-h6">Project X</div>
+      </q-img>
+
+      <q-card-section>
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<style>
+.my-card {
+  width: 100%;
+  max-width: 350px;
+}
+</style>
+
+<script setup>
+const lorem =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+const faultDetectorAPP = {
+  name: 'Fault Detector',
+  description:
+    'Web App for visualization of fault detection strategy based on harmonic distortion as part of research project INTEGRA and my MSc thesis.\n \n Vanilla JS + Django-Ninja + NumPy Stack '
+}
+const finaleAPP = {
+  name: 'Finale App',
+  description:
+    'Web App designed for football team management, simplifying team members’ data collection. Frontend and Backend conteinerized, togheter with DB hosted in Azure. Quasar (VueJs) + ASP.NET + docker-compose + Azure + PostgreSQL'
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-page class="column flex flex-center justify-around">
+    <div class="column">
+      <q-btn round>
+        <q-avatar size="350px" round>
+          <img src="~assets/profilePicture3.png" alt="profile-picture" />
+        </q-avatar>
+      </q-btn>
+      <q-item style="padding-left: 0">
+        <q-item-section>
+          <q-item-label>{{ personalInfo.name }}</q-item-label>
+          <q-item-label caption lines="2">{{
+            personalInfo.career
+          }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </div>
+    <div style="width: 80%">
+      <ProjectsCards></ProjectsCards>
+    </div>
+
+    <!-- <div class="column justify-around flex-center">
+      <q-img
+        alt="profilePicture"
+        src="~assets/profilePicture3.png"
+        spinner-color="blue"
+        width="350px"
+        :img-style="{ borderRadius: '50%' }"
+      />
+      <div class="text-h4" style="padding-top: 10px">
+        {{ personalInfo.name }}
+      </div>
+      <div class="text-caption">{{ personalInfo.career }}</div>
+    </div> -->
+  </q-page>
+</template>
+
+<script setup>
+import ProjectsCards from 'components/ProjectsCards.vue'
+import { ref } from 'vue'
+const personalInfo = ref([])
+
+personalInfo.value.name = 'Alvaro Herrada Coronell'
+personalInfo.value.career = 'Electrical Engineer'
+</script>

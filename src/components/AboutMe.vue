fe14eb6d4f04a591840b36055e86f43b372c2f95
alvarohc777 <aherrada@uninorte.edu.co>
2024-01-20 20:15:12 -0500

    Put aboutme section into a component

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-list class="rounded-borders" style="max-width: 600px">
+    <q-item clickable href="https://www.bluetab.net/es/" target="_blank">
+      <q-item-section avatar top>
+        <q-icon color="black" size="34px">
+          <img src="~assets/bluetab.jpg" alt="bluetabLogo" />
+        </q-icon>
+      </q-item-section>
+
+      <q-item-section top>
+        <q-item-label lines="1">
+          <span class="text-weight-medium">Data Technician</span>
+          <span class="text-grey-8"> - Bluetab, an IBM company</span>
+        </q-item-label>
+        <q-item-label caption lines="1"> Barranquilla, Colombia </q-item-label>
+        <q-item-label
+          lines="1"
+          class="q-mt-xs text-body2 text-weight-bold text-primary text-uppercase"
+        >
+        </q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-item clickable href="https://www.uninorte.edu.co/" target="_blank">
+      <q-item-section avatar top>
+        <q-icon color="black" size="34px">
+          <img src="~assets/uninorteLogo.jpg" alt="uninorteLogo" />
+        </q-icon>
+      </q-item-section>
+
+      <q-item-section top>
+        <q-item-label lines="1">
+          <span class="text-weight-medium">Electrical Engineering</span>
+          <span class="text-grey-8"> - Universidad del Norte</span>
+        </q-item-label>
+        <q-item-label caption lines="1"> Barranquilla, Colombia </q-item-label>
+        <q-item-label
+          lines="1"
+          class="q-mt-xs text-body2 text-weight-bold text-primary text-uppercase"
+        >
+        </q-item-label>
+      </q-item-section>
+    </q-item>
+    <!-- <q-separator spaced /> -->
+  </q-list>
+</template>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -34,53 +34,9 @@
       <div class="q-pa-md row justify-between q-gutter-md">
         <q-item-label overline style="font-size: 20px">About me</q-item-label>
       </div>
-      <q-list class="rounded-borders" style="max-width: 600px">
-        <q-item>
-          <q-item-section avatar top>
-            <q-icon color="black" size="34px">
-              <img src="~assets/bluetab.jpg" alt="bluetabLogo" />
-            </q-icon>
-          </q-item-section>
-
-          <q-item-section top>
-            <q-item-label lines="1">
-              <span class="text-weight-medium">Data Technician</span>
-              <span class="text-grey-8"> - Bluetab, an IBM company</span>
-            </q-item-label>
-            <q-item-label caption lines="1">
-              Barranquilla, Colombia
-            </q-item-label>
-            <q-item-label
-              lines="1"
-              class="q-mt-xs text-body2 text-weight-bold text-primary text-uppercase"
-            >
-            </q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item>
-          <q-item-section avatar top>
-            <q-icon color="black" size="34px">
-              <img src="~assets/uninorteLogo.jpg" alt="uninorteLogo" />
-            </q-icon>
-          </q-item-section>
-
-          <q-item-section top>
-            <q-item-label lines="1">
-              <span class="text-weight-medium">Electrical Engineering</span>
-              <span class="text-grey-8"> - Universidad del Norte</span>
-            </q-item-label>
-            <q-item-label caption lines="1">
-              Barranquilla, Colombia
-            </q-item-label>
-            <q-item-label
-              lines="1"
-              class="q-mt-xs text-body2 text-weight-bold text-primary text-uppercase"
-            >
-            </q-item-label>
-          </q-item-section>
-        </q-item>
-        <!-- <q-separator spaced /> -->
-      </q-list>
+      <AboutMe></AboutMe>
+      <!-- <div class="q-pa-md row justify-start q-gutter-md">
+        <AboutMe></AboutMe>
     </div>
     <div id="projects" class="section-container">
       <q-separator inset />
@@ -112,6 +68,7 @@
 </style>
 <script setup>
 import ProjectsCards from 'components/ProjectsCards.vue'
+import AboutMe from 'components/AboutMe.vue'
 import { ref } from 'vue'
 const personalInfo = ref([])
 
